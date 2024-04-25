c36c673922b2a4d5a64e214566721c754a091cbf
chucky-golden <chukschibyke92@gmail.com>
2024-04-25 03:50:01 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+require('dotenv').config()
+import express from "express"
+import cors from 'cors'
+import proxy from 'express-http-proxy'
+
+const PORT: any = process.env.PORT
+const app: any = express()
+
+
+app.use(cors())
+
+
+app.use('/drivers/api/v1', proxy('https://dooryd-api-drivers.onrender.com'))
+app.use('/users/api/v1', proxy('https://admin-dqcw.onrender.com'))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
+app.listen(PORT, () => {
+    console.log(`gateway running... on port ${PORT}`)
+})
